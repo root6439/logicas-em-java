644823b115cbd5be37bc05d6b7a23ec77f7f5913
root6439 <nicolas.soaresh.98@gmail.com>
2019-08-21 17:43:22 -0300

    Histograma

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,59 @@
 package application;
 
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		int[] vetor = {3, 4, 0, 3, 5, 0, 0, 2, 4, 6};
+				
+		int[] histograma = new int[vetor.length];
 		
-		System.out.print("Número de elementos: ");
-		int tamanho = sc.nextInt();
-		sc.nextLine();
+		organizaVetor(vetor);
 		
-		int[] array = new int[tamanho];
-		
-		System.out.println();
-		for (int i = 0; i < tamanho; i++) {
-			array[i] = i + 1;
-			System.out.print(array[i] + " ");
+		for(int i = 0; i < histograma.length; i++) {
+			if(existeNoVetor(i, vetor)) {
+				histograma[i] = 1;
+			} else {
+				histograma[i] = 0;
+			}
 		}
-		
-		int[] antiarray = new int[tamanho];
-		
-		System.out.println();
-		for (int i = 0; i < tamanho; i++) {
-			antiarray[i] = array[(tamanho - 1) - i];
-			System.out.print(antiarray[i] + " ");
+		for(int i = 0; i < vetor.length; i++) {
+			for(int j = i + 1; j < vetor.length; j++) {
+				if(vetor[i] == vetor[j] && histograma[i] >= 1) {
+					histograma[i]++;
+				}
+			}
 		}
-		
-		sc.close();
+				
+		for(int num : vetor) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+		for(int num : histograma) {
+			System.out.print(num + " ");
+		}
 	}
-
+	
+	public static boolean existeNoVetor(int numero, int vetor[]) {
+		boolean existe = false;
+		for(int i = 0; i < vetor.length; i++) {
+			if(numero == vetor[i]) {
+				existe = true;
+			}
+		}
+		return existe;
+	}
+	public static void organizaVetor(int vetor[]) {
+		int aux;
+		for(int i = 0; i<vetor.length; i++){
+	        for(int j = 0; j < vetor.length - 1; j++){
+	            if(vetor[j] > vetor[j + 1]){
+	                aux = vetor[j];
+	                vetor[j] = vetor[j+1];
+	                vetor[j+1] = aux;
+	            }
+	        }
+	    }
+	}
+	
 }
