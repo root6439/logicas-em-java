5592d02484e44751854271e29eab96719fb8889a
root6439 <nicolas.soaresh.98@gmail.com>
2019-08-21 17:52:38 -0300

    Multiplicação de matrizes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,54 +1,33 @@
 package application;
 
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		int[][] matriz1 = {{1, 0, -1}, {3, 2, 5}};
+		int[][] matriz2 = {{1, 0, -1, 1}, {0, 0, 0, 2}, {2, 1, 0, 3}};
+		int[][] matriz3 = new int[matriz1.length][matriz2[0].length];
+		int aux = 0;
 		
-		System.out.print("Número de linhas da matriz: ");
-		int lines = sc.nextInt();
-		sc.nextLine();
 		
-		System.out.print("Número de colunas: ");
-		int columns = sc.nextInt();
-		sc.nextLine();
-		
-		int[][] matriz = new int[lines][columns];
-
-		for(int i = 0; i < lines; i++) {
-			for(int j = 0; j < columns; j++) {
-				matriz[i][j] = i + j; 
-			}
-		}
-
-		System.out.println();
-		for(int i = 0; i < lines; i++) {
-			for(int j = 0; j < columns; j++) {
-				System.out.print(matriz[i][j] + " "); 
-			}
-			System.out.println();
-		}
-		
-		int[][] transposta = new int[columns][lines];
-		
-		for(int i = 0; i < columns; i++) {
-			for(int j = 0; j < lines; j++) {
-				transposta[i][j] = matriz[j][i]; 
+		for(int i = 0; i < matriz1.length; i++) {
+			for(int j = 0; j < matriz2[0].length; j++) {
+				while(aux < matriz1[0].length && aux < matriz2.length) {
+					matriz3[i][j] += matriz1[i][aux] * matriz2[aux][j];
+					aux++;
+				}
+				aux = 0;
 			}
 		}
 		
 		System.out.println();
-		for(int i = 0; i < columns; i++) {
-			for(int j = 0; j < lines; j++) {
-				System.out.print(transposta[i][j] + " "); 
+		for(int i = 0; i < matriz3.length; i++) {
+			for(int j = 0; j < matriz3[0].length; j++) {
+				System.out.print(matriz3[i][j] + " ");
 			}
 			System.out.println();
 		}
 		
-		sc.close();
 	}
-
+	
 }
